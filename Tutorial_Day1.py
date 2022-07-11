cf6573475543ae28c6925a8c8f752bfc91e32c25
MarcioRB <66230883+MRB-2020@users.noreply.github.com>
2022-07-11 19:43:27 -0400

    Create Tutorial_Day1.py

---
+++ Tutorial_Day1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 18:28:25 2022
+
+@author: marciobernardo
+"""
+
+## FreeCodeCamp Tutorial - day 1
+
+# Start at 6:41
+# Using Spyder IDE
+
+
