d59b72d7eb8591da981afd38acead40760b3e80d
MarcioRB <66230883+MRB-2020@users.noreply.github.com>
2022-07-12 17:26:26 -0400

    Create Tutorial_Day1_2.py

---
+++ Tutorial_Day1_2.py
@@ -0,0 +1,7 @@
+## User input
+
+name = input('Enter your name: ')
+
+course = input(f'Hello {name},\nwhat Springboard course are you doing? ')
+
+print(f'{course} is challenging but it will be worth it.\nGood luck on your studies {name}!')
